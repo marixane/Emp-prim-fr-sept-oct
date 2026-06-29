7c6db9caf21d36cc6d16984e733dbfa7ad4f90e2
marixane <prof.marwane@gmail.com>
2026-06-29 02:08:38 +0100

    Import opaque photo controls styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './compact-exercise.css';
 import './test-title.css';
 import './dark-background.css';
+import './photo-controls-opaque.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
